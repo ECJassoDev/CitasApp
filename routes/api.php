1b437e2e18e6783bff0a82977b356438b9e50235
Emanuel Campos Jasso <62405903+EmanuelSulivan@users.noreply.github.com>
2021-01-15 18:33:19 -0600

    test de nueo projecto

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('test', function () {
-    return 'Hola cara de bola';
+    return 'Hola cara de bola nuevo';
 });
